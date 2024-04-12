0655c2d39f4a36619b7c9f026b99af9b085a21de
张新阳 <1623190186@qq.com>
2024-04-12 14:51:21 +0800

    feat:首页布局

---
+++ packages/icons/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import "./index.scss";
+
+export const HomePage = () => {
+  return (
+    <div className="z-icon-home">
+      <div className="z-home-header">Header</div>
+      <div className="z-home-container">Container</div>
+      <div className="z-home-footer">Footer</div>
+    </div>
+  );
+};
