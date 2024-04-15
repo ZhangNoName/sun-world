a00f420aff5af4fce43a9e966017e56f1938e362
zxy <1623190186@qq..com>
2024-04-16 07:46:33 +0800

    feat:优化icons布局

--- packages/icons/src/icons/AddOutlined.tsx
+++ packages/icons/src/icons/AddOutlined.tsx
@@ -1,13 +1,3 @@
-/*
- * @Author: ZhangNoName
- * @Date: 2024-04-11 22:41:03
- * @LastEditors: zxy 1623190186@qq..com
- * @LastEditTime: 2024-04-11 22:44:38
- * @FilePath: \sun-world\packages\icons\src\icons\add-outlined.tsx
- * @Description:
- *
- * Copyright (c) 2024 by ZhangNoName, All Rights Reserved.
- */
 import React from "react";
 import { ZIconProps } from "../type";
 import { DEFAULT_PARAMS } from "../data";
--- packages/icons/src/pages/index.tsx
+++ packages/icons/src/pages/index.tsx
@@ -1,12 +1,19 @@
+import { isValidElement } from "react";
 import { ZIconCard } from "../components/card";
+import { IconsList, initIcon } from "./index.data";
 import "./index.scss";
 
 export const HomePage = () => {
+  console.log("icons列表", IconsList);
   return (
     <div className="z-icon-home">
       <div className="z-home-header">Header</div>
       <div className="z-home-content">
-        <ZIconCard />
+        {IconsList.map((Item, index) => {
+          console.log(index, isValidElement(Item));
+          // return <ZIconCard key={index} icon={initIcon(item, {})}></ZIconCard>;
+          return <Item></Item>;
+        })}
       </div>
       <div className="z-home-footer">Footer</div>
     </div>
