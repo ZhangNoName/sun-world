5f18e56e214a532da9e479340918198c99736dcd
zxy <1623190186@qq..com>
2024-08-07 23:11:40 +0800

    feat:添加保存按钮

--- packages/blog/src/blogEditor/index.ts
+++ packages/blog/src/blogEditor/index.ts
@@ -2,11 +2,20 @@ import { BlogConfig, BlogConfigType } from './config'
 import Vditor from 'vditor'
 import 'vditor/dist/index.css'
 export class BlogEditorClass {
-  public containerEle: HTMLElement
-  public config: BlogConfigType
-  public blogEditor: Vditor
+  public containerEle: HTMLElement | null = null
+  public config: BlogConfigType | null = null
+  public blogEditor: Vditor | null = null
 
-  constructor(ele: HTMLElement) {
+  constructor(ele?: HTMLElement) {
+    if (ele) {
+      this.init(ele)
+    }
+  }
+  /**
+   * 初始化编辑器容器，避免vue在初始化的时候ref要考虑为null
+   * @param ele 编辑器容器元素
+   */
+  init = (ele: HTMLElement) => {
     this.containerEle = ele
     this.config = new BlogConfig()
     this.blogEditor = new Vditor(this.containerEle, {
@@ -23,6 +32,13 @@ export class BlogEditorClass {
     })
   }
 
+  /**
+   * 获取编辑器内容，方便保存到后端
+   * @returns 编辑器内容
+   */
+  getContent = () => {
+    return this.blogEditor?.getValue()
+  }
   /**
    * 保存当前编辑器信息
    */
--- packages/blog/src/pages/article/index.vue
+++ packages/blog/src/pages/article/index.vue
@@ -1,16 +1,24 @@
 <script lang="ts" setup>
 import { onMounted, ref } from 'vue'
-import SelfInfoCard from '@/components/SelfInfoCard/index.vue'
 import ZHeader from '@/layout/header/index.vue'
-import ZFooter from '@/layout/footer/index.vue'
 import { BlogEditorClass } from '@/blogEditor'
-
+import { ElMessage } from 'element-plus'
+import ZBtn from '@/components/ZBtn/index.vue'
 const prop = defineProps()
 
 const editorEle = ref()
+const blogEditor = ref<BlogEditorClass>(new BlogEditorClass())
+const saveBlog = () => {
+  console.log(blogEditor.value.getContent())
+  console.log('saveBlog')
+  ElMessage.success('保存成功')
+  // blogEditor?.value.save()
+}
 onMounted(() => {
-  const blogEditor = new BlogEditorClass(editorEle.value)
-  console.log(blogEditor)
+  blogEditor.value.init(editorEle.value)
+  // console.log(blogEditor)
+  // 将blogEditor挂载到全局
+  ;(window as any).blogEditor = blogEditor
 })
 </script>
 
@@ -19,6 +27,10 @@ onMounted(() => {
     <ZHeader></ZHeader>
 
     <div class="content">
+      <div class="func-bar">
+        <div class="stastic">统计信息</div>
+        <div class="btn-container"><ZBtn @click="saveBlog">保存</ZBtn></div>
+      </div>
       <div ref="editorEle" class="editor-container"></div>
     </div>
   </div>
@@ -33,13 +45,20 @@ onMounted(() => {
     grid-template-columns: auto;
     grid-template-rows: auto;
     gap: 1rem;
-    .left {
+    .func-bar {
+      height: 3rem;
+      width: 100%;
       display: flex;
-      flex-direction: column;
-      gap: 1.5rem;
-    }
-    .right {
-      // background-color: blue;
+      align-items: center;
+      .stastic {
+        flex: 1;
+      }
+      .btn-container {
+        display: flex;
+        justify-content: flex-end;
+        gap: 1rem;
+        align-items: center;
+      }
     }
   }
 }
