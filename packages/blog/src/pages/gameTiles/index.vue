66bca9c58efbe80606dfc775d1446c715c51e2d5
张新阳 <1623190186@qq.com>
2024-12-30 17:24:55 +0800

    feat:完善文件上传

--- packages/blog/src/pages/gameTiles/index.vue
+++ packages/blog/src/pages/gameTiles/index.vue
@@ -1,5 +1,16 @@
 <script lang="ts" setup>
-import { ElInputNumber, ElOption, ElSelect } from 'element-plus'
+import {
+  ElDialog,
+  ElIcon,
+  ElInputNumber,
+  ElOption,
+  ElSelect,
+  ElUpload,
+  UploadProps,
+  UploadRequestHandler,
+  UploadUserFile,
+} from 'element-plus'
+import { AddSvg } from '@sun-world/icons-vue'
 import { computed } from 'vue'
 import { reactive } from 'vue'
 import { ref } from 'vue'
@@ -12,12 +23,62 @@ const tileConfig = reactive({
   height: 16,
 })
 const renderType = ref('div')
+const dialogImageUrl = ref('')
+const dialogVisible = ref(false)
+const fileList = ref<UploadUserFile[]>([
+  {
+    name: 'food.jpeg',
+    url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100',
+  },
+  {
+    name: 'plant-1.png',
+    url: '/images/plant-1.png',
+  },
+])
 const canvasConfig = computed(() => {
   return {
     width: tileConfig.row * tileConfig.width,
     height: tileConfig.col * tileConfig.height,
   }
 })
+
+const handleRemove: UploadProps['onRemove'] = (uploadFile, uploadFiles) => {
+  console.log(uploadFile, uploadFiles)
+}
+const handlePictureCardPreview: UploadProps['onPreview'] = (uploadFile) => {
+  dialogImageUrl.value = uploadFile.url!
+  dialogVisible.value = true
+}
+// 自定义上传请求
+// 阻止上传，只有读取文件
+const handleBeforeUpload = (file: File) => {
+  // 读取文件
+  readFile(file)
+  // 返回 false，表示不上传文件
+  return false
+}
+// 读取文件信息并生成图片预览
+const readFile = (file: File) => {
+  // 使用 FileReader 读取文件
+  const reader = new FileReader()
+
+  reader.onload = () => {
+    // 生成一个 URL，方便 img 显示图片
+    const fileUrl = reader.result as string
+    fileList.value.push({
+      name: file.name,
+      size: file.size,
+      // type: file.type,
+      url: fileUrl, // 将文件的URL保存到 file.url
+    })
+  }
+
+  reader.onerror = (error) => {
+    console.error('Error reading file:', error)
+  }
+
+  reader.readAsDataURL(file) // 读取文件为 data URL
+}
 </script>
 
 <template>
@@ -74,6 +135,27 @@ const canvasConfig = computed(() => {
         </div>
         <div class="config-item">
           <div class="label">导入已有tiles：</div>
+          <ElUpload
+            v-model:file-list="fileList"
+            drag
+            :on-preview="handlePictureCardPreview"
+            :on-remove="handleRemove"
+            :before-upload="handleBeforeUpload"
+            list-type="picture-card"
+          >
+            <!-- c -->
+
+            <AddSvg width="2.5rem" height="2.5rem" />
+
+            <div class="upload-text">
+              拖动文件到此处或者
+              <em>点击</em>
+              上传
+            </div>
+          </ElUpload>
+          <ElDialog v-model="dialogVisible">
+            <img w-full :src="dialogImageUrl" alt="Preview Image" />
+          </ElDialog>
         </div>
         <div class="config-item"></div>
         <div class="config-item"></div>
@@ -133,6 +215,10 @@ const canvasConfig = computed(() => {
           align-items: center;
           gap: 0.5rem;
         }
+      }
+      .el-upload-dragger {
+        width: 100%;
+        height: 100%;
       }
     }
   }
